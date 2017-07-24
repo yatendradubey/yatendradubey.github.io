49d59494b3d4be9696ee1e78e28f008335365079
yatendradubey <yatendradubey.16@gmail.com>
2017-07-24 19:31:02 +0530

    new3

--- login.php
+++ login.php
@@ -3,26 +3,13 @@ include("connect.php");
 
 $sql="SELECT email,password FROM signup WHERE (email='$_POST[email]' AND password='$_POST[password]')";
 
-/*if ($result=mysqli_query($conn,$sql))
- {
-  // Fetch one and one row
-  while ($row=mysqli_fetch_array($result))
-    {
-    echo $row['email'].'&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp '.$row['password'].'<br/>';
-    }
-  // Free result set
-  mysqli_free_result($result);
-}
-else {
-	echo "Data not fetched";
-}*/
 if($result=mysqli_query($conn,$sql)){
 	if($row=mysqli_fetch_array($result)){
 	echo "Logged in successfully";
-	header("Refresh: 1; url=html/login.html");
+	header("Refresh: 2; url=html/login.html");
 	}
 	else{
-		die("Incorrect email and password..<br />Please enter correct Details..<br />Redirecting to Login page");
+		echo "Incorrect email and password..<br />Please enter correct Details..<br />Redirecting to Login page";
 		header("Refresh: 2; url=html/login.html");
 	}
 }
