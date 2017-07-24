1e5e461c08efd1f78301430636a4fadc8a5a3327
yatendradubey <yatendradubey.16@gmail.com>
2017-07-24 19:00:24 +0530

    new2

---
+++ gitignore.md
@@ -0,0 +1,1 @@
+This is a shopping website project.10
--- signup.php
+++ signup.php
@@ -1,10 +1,11 @@
 <?php
 include("connect.php");
-$sql = "INSERT INTO signup (name, email, password)
-VALUES ('$_POST[name]','$_POST[email]','$_POST[password]')";
+$sql = "INSERT INTO signup (name, email, password,mobile_no)
+VALUES ('$_POST[name]','$_POST[email]','$_POST[password]','$_POST[mobile_no]')";
 
 if (mysqli_query($conn, $sql)) {
     echo "New record created successfully";
+	echo "<br />";
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
